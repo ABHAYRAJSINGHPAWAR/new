264daf42c22f818771ef31f447ebf02bb864fb3e
bhayajingh <abhimanyusinghpawar1105@gmail.com>
2023-10-09 18:25:44 +0530

    runnable file

---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import {Navbar  } from "react-bootstrap";
+import {FaUser,FaHome } from "react-icons/fa";
+
+const Header = () => {
+    return (
+        <Header>
+        <Navbar bg="light" variant="dark" expand="lg" collapseOnSelect></Navbar>
+        </Header>
+        
+    );
+}
+
+export default Header;
